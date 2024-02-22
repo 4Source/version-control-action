d1d155d47b07a50026efacd6390c7b531eedbd3c
4Source <38220764+4Source@users.noreply.github.com>
2024-02-22 15:49:15 +0100

    Test Tag fetch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 const github = require('@actions/github');
+const semver = require('semver');
 
 /**
  * The main function for the action.
@@ -7,72 +9,81 @@ const github = require('@actions/github');
  */
 async function run() {
   try {
-    const owner = core.getInput('owner', { required: true });
-    const repo = core.getInput('repo', { required: true });
-    const pr_number = core.getInput('pr_number', { required: true });
-    const token = core.getInput('token', { required: true });
+    const github_token = core.getInput('github_token', { required: true });
+    const label_major = core.getInput('label_major', { required: false });
+    const label_minor = core.getInput('label_minor', { required: false });
+    const label_patch = core.getInput('label_patch', { required: false });
+    const label_beta = core.getInput('label_beta', { required: false });
+    const label_alpha = core.getInput('label_alpha', { required: false });
+    const label_docs = core.getInput('label_docs', { required: false });
+    const tag_prefix = core.getInput('tag_prefix', { required: false });
+    const dry_run = core.getInput('dry_run', { required: false });
 
-    const octokit = new github.getOctokit(token);
+    const { GITHUB_REF, GITHUB_SHA } = process.env;
 
-    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
-      owner,
-      repo,
-      pull_number: pr_number
-    });
-
-    let diffData = {
-      additions: 0,
-      deletions: 0,
-      changes: 0
-    };
-
-    diffData = changedFiles.reduce((acc, file) => {
-      acc.additions += file.additions;
-      acc.deletions += file.deletions;
-      acc.changes += file.changes;
-      return acc;
-    }, diffData);
-
-    await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pr_number,
-      body: `
-      Pull request #${pr_number} has be updated with: \n
-      - ${diffData.additions} additions \n
-      - ${diffData.deletions} deletions \n
-      - ${diffData.changes} changes \n
-      `
-    });
-
-    for (const file of changedFiles) {
-      const fileExtension = file.filename.split('.').pop();
-      let label = '';
-      switch (fileExtension) {
-        case 'md':
-          label = 'markdown';
-          break;
-        case 'js':
-          label = 'javascript';
-          break;
-        case 'ts':
-          label = 'typescript';
-          break;
-        case '':
-          label = 'noextension';
-          break;
-        default:
-          label = 'nomatch';
-          break;
-      }
-
-      await octokit.rest.issues.addLabels({
-        owner,
-        repo,
-        issue_number: pr_number,
-        labels: [label]
-      });
+    if (!GITHUB_REF) {
+      core.setFailed('Missing GITHUB_REF');
+      return;
     }
+
+    if (!GITHUB_SHA) {
+      core.setFailed('Missing GITHUB_SHA');
+      return;
+    }
+
+    core.debug(GITHUB_REF);
+    core.debug(GITHUB_SHA);
+
+    await exec('git fetch --tags');
+
+    const hasTag = !!(await exec('git tag')).stdout.trim();
+    let tag = '';
+
+    if (hasTag) {
+      const previousTagSha = (
+        await exec('git rev-list --tags --topo-order --max-count=1')
+      ).stdout.trim();
+      tag = (await exec(`git describe --tags ${previousTagSha}`)).stdout.trim();
+
+      core.debug(`Previous tag is: ${tag}`);
+
+      if (previousTagSha === GITHUB_SHA) {
+        core.debug('No new commits since previous tag. Skipping...');
+        return;
+      }
+    } else {
+      tag = '0.0.0';
+
+      core.debug('No previous tag.');
+    }
+
+    // // for some reason the commits start and end with a `'` on the CI so we ignore it
+    // const commits = logs
+    //   .split(SEPARATOR)
+    //   .map((x) => ({
+    //     message: x.trim().replace(/^'\n'/g, "").replace(/^'/g, ""),
+    //   }))
+    //   .filter((x) => !!x.message);
+    // const bump = await analyzeCommits(
+    //   {},
+    //   { commits, logger: { log: console.info.bind(console) } }
+    // );
+
+    // if (!bump) {
+    //   core.debug("No commit specifies the version bump. Skipping...");
+    //   return;
+    // }
+    // inc(tag, )
+
+    // const newVersion = `${inc(tag, bump || defaultBump)}${
+    //   preRelease ? `-${GITHUB_SHA.slice(0, 7)}` : ""
+    // }`;
+    // const newTag = `${tagPrefix}${newVersion}`;
+
+    // core.setOutput("new_version", newVersion);
+    // core.setOutput("new_tag", newTag);
+
+    // core.debug(`New tag: ${newTag}`);
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message);
